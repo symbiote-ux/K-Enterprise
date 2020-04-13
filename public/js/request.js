89154242c0f6ccd89190c083fc677d48ca82ba2b
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-04-13 16:09:54 +0530

    fixed css

--- public/js/request.js
+++ public/js/request.js
@@ -1,3 +1,7 @@
+const saveValue = value => {
+  fetch('/saveValue',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify(value)});
+}
+
 const productDetails = () => {
   fetch('/productDetails')
     .then(res => res.json())
--- public/js/template.js
+++ public/js/template.js
@@ -4,7 +4,7 @@ const showProductDetails = details => {
     for(const key in details[product]) {
       const box = document.createElement('div');
       box.innerHTML = `<div class="name">${key}</div>
-      <div class="value" contentEditable="true">${details[product][key]}</div>`;
+      <input class="value" id="" value="${details[product][key]}" onchange="saveValue(this.value)"/>`;
       box.className = 'unit';
       infoBox.appendChild(box);
     }
