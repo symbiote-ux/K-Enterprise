69e88b4449df238f911bb7ad31d77ab4b143c756
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-07-23 18:08:12 +0530

    removed addCLient

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,3 @@
-const createAddClientBox = () => {
-  const addClientBox = document.querySelector('.addClient');
-  const name = document.createElement('input');
-  const button = document.createElement('button');
-  button.innerText = '+';
-  button.id = 'add';
-  name.id = 'clientName';
-  name.placeholder = 'Client Name...';
-  addClientBox.appendChild(name);
-  addClientBox.appendChild(button);
-};
-
 const createProductBox = () => {
   const productList = [ 'Size_1', 'Size_2', 'Size_3', 'Size_4', 'Size_5', 'Size_6' ];
   const productBox = document.querySelector('.productBox');
@@ -23,7 +11,6 @@ const createProductBox = () => {
 
 const main = () => {
   createProductBox();
-  createAddClientBox();
   productDetails();
   clientsDetails();
 };
